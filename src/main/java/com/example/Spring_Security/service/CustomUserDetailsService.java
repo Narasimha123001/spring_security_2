5f0f42278bd4dbfa509edcb27fc0f774da3aa861
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-09-19 09:47:57 +0530

    "feat: create CustomUserDetailsService for user authentication"

---
+++ src/main/java/com/example/Spring_Security/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.Spring_Security.service;
+
+
+import com.example.Spring_Security.repo.UserDetailsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+
+    @Autowired
+    private UserDetailsRepository userDetailsRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userDetailsRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username Not Found"));
+    }
+
+
+}
