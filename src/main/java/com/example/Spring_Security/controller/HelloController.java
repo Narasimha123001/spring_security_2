57486d4713ff7208b673d848522ae1e0676b5eb7
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-09-22 11:06:01 +0530

    "feat: Add preAuthorize for the method"

--- src/main/java/com/example/Spring_Security/controller/HelloController.java
+++ src/main/java/com/example/Spring_Security/controller/HelloController.java
@@ -1,23 +1,27 @@
 package com.example.Spring_Security.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Scanner;
 
 @RestController
 @RequestMapping("/happy")
 public class HelloController {
 
+
+    @PreAuthorize("hasAnyRole('ADMIN' , 'USER')")
     @GetMapping("hi")
     public String hello(){
         return "hello rey";
     }
 
+    @PreAuthorize("hasRole('ADMIN' )")
     @GetMapping("/bye")
     public String bye(){
         return "bye";
     }
 
+    @PreAuthorize("hasRole('USER')")
    @PostMapping("/message")
     public String message(@RequestBody  String message){
         return message;
